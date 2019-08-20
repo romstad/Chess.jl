a30d8f7720ea4d4ba9bf4911e73f0bbd67bbfe40
Tord Romstad <tord.romstad@gmail.com>
2019-08-20 16:53:24 +0200

    Cosmetic documentation fix.

--- src/square.jl
+++ src/square.jl
@@ -190,7 +190,7 @@ end
 
 Type representing a square on a chess board.
 
-A `Square` can be constructed either with an Int (with the convention a8=1,
+A `Square` can be constructed either with an `Int` (with the convention a8=1,
 a7=2, ..., a1=8, b8=9, b7=10, ..., h1=64) or with a `SquareFile` and a
 `SquareRank`. There are also constants `SQ_A1`, ..., `SQ_H8` for all 64
 squares on the board.
