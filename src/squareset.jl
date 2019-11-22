6dccd8fcb1cfa0f2ea844da453a0ce69221fb9bb
Tord Romstad <tord.romstad@gmail.com>
2019-11-23 00:15:54 +0100

    Add an optional element type to toarray(::SquareSet).

--- src/squareset.jl
+++ src/squareset.jl
@@ -523,9 +523,9 @@ end
 
 
 """
-    toarray(ss::SquareSet)
+    toarray(ss::SquareSet, T::Type{<:Number}=Float32)
 
-Convert a square set to a two-dimensional array.
+Convert a square set to a two-dimensional array of the specified element type.
 
 The returned array's columns corresponds to the files, and the rows to the
 ranks. The array entries are 1 for the members of the square set, and 0
@@ -534,7 +534,7 @@ for non-members.
 # Examples
 
 ```julia-repl
-julia> toarray(SS_FILE_C ∪ SS_RANK_5)
+julia> toarray(SS_FILE_C ∪ SS_RANK_5, Int)
 8×8 Array{Int64,2}:
  0  0  1  0  0  0  0  0
  0  0  1  0  0  0  0  0
@@ -546,8 +546,8 @@ julia> toarray(SS_FILE_C ∪ SS_RANK_5)
  0  0  1  0  0  0  0  0
 ```
 """
-function toarray(ss::SquareSet)::Array{Int, 2}
-    reshape([Square(i) ∈ ss ? 1 : 0 for i in 1:64],
+function toarray(ss::SquareSet, T::Type{<:Number}=Float32)::Array{T, 2}
+    reshape([Square(i) ∈ ss ? one(T) : zero(T) for i in 1:64],
             8, 8)
 end
 
