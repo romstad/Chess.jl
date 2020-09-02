9773197424986bb40beb4a1652c3b17e77cf5dcd
Tord Romstad <tord.romstad@gmail.com>
2020-09-02 14:01:38 +0200

    Changed to MIT license.

--- test/square.jl
+++ test/square.jl
@@ -1,21 +1,3 @@
-#=
-        Chess.jl: A Julia chess programming library
-        Copyright (C) 2019 Tord Romstad
-
-        This program is free software: you can redistribute it and/or modify
-        it under the terms of the GNU Affero General Public License as
-        published by the Free Software Foundation, either version 3 of the
-        License, or (at your option) any later version.
-
-        This program is distributed in the hope that it will be useful,
-        but WITHOUT ANY WARRANTY; without even the implied warranty of
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        GNU Affero General Public License for more details.
-
-        You should have received a copy of the GNU Affero General Public License
-        along with this program.  If not, see <https://www.gnu.org/licenses/>.
-=#
-
 const ALL_FILES = [FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H]
 const ALL_RANKS = [RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8]
 
